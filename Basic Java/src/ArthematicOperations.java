f85f0369f65fc79166d6d77a7ff003b7fad80603
Manohar <manoharchoragudi@outlook.com>
2021-01-18 19:47:02 +0530

    commit

--- Basic Java/src/ArthematicOperations.java
+++ Basic Java/src/ArthematicOperations.java
@@ -1,6 +1,18 @@
 
-
+import java.util.Scanner;
 public class ArthematicOperations {
-    int a,b;
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter two Numbers:");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("Sum="+(a+b));
+        System.out.println("Difference="+(a-b));
+        System.out.println("Multiplication="+a*b);
+        System.out.println("Modulus="+(a%b));
+    }
+
 
 }
